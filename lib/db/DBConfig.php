09d81eb0fde354e19071065326f9e4597d6b7016
root <root@centos72.example.com>
2017-08-09 14:46:53 +0800

    add

---
+++ lib/db/DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * DB配置
+ */
+ class DBConfig
+{
+    const HOST = 'localhost';
+    const PORT = '3306';
+    const USER = 'your-user-name';
+    const PASSWORD = 'your-pass-word';
+    // DB NAME
+    const DATABASE = 'sxb_db';
+    const CHARSET = 'utf8mb4';
+}
+?>
