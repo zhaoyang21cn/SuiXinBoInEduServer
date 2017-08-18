5de482e4553d532b5ed2044f94f47d476302c917
tianyaxingke <root@centos72.example.com>
2017-08-18 17:07:06 +0800

    增加sdkappid对应管理账号的判断

--- Config.php
+++ Config.php
@@ -8,15 +8,15 @@ require_once dirname(__FILE__) . '/Path.php';
 define('GLOBAL_CONFIG_APP_ID', 'Your_APP_ID'); //AppID
 define('GLOBAL_CONFIG_SECRET_ID', 'Your_SECRET_ID'); //SECRET_ID
 define('GLOBAL_CONFIG_SECRET_KEY', 'Your_SECRET_KEY'); //SECRET_KEY
+//各sdkappid的管理员账号
+define('GLOBAL_CONFIG_SDK_ADMIN', serialize([
+    'Your_SDK_APP_ID' => '[admin_name]'
+]));
 
 //cos
 define('GLOBAL_CONFIG_COS_BUCKET', 'Your_COS_BUCKET'); //bucket
 define('GLOBAL_CONFIG_COS_REGION', 'Your_COS_REGION'); //设置COS所在的区域 华南  -> gz;华东  -> sh;华北  -> tj
 define('GLOBAL_CONFIG_COS_SIG_EXPIRATION',2592000 ); //签名有效期,单位:秒,默认值30*24*3600
 define('GLOBAL_CONFIG_COS_PREVIEW_TAG','preview' ); //文档预览域名[bucket]-[appid].[preview_tag].myqcloud.com中的[preview_tag]号部分
-
-define('AUTHORIZATION_KEY', serialize([
-    'Your_SDK_APP_ID' => 'Your_Authrization_Key'
-])); //权限密钥表
 
 ?>
